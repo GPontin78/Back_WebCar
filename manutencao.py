143941f7a853e89acf17e0945739a477bb4bb4da
GPontin78 <gustavopontin02@gmail.com>
2026-04-07 17:26:34 -0300

    Cadastro adicionar item manutancao
    Criação das rotas de adicionar item manutencao

--- manutencao.py
+++ manutencao.py
@@ -140,15 +140,141 @@ def buscar_servico():
     finally:
         cursor.close()
 
+@app.route('/adicionar_manutencao', methods=['POST'])
+def adicionar_manutencao():
+    dados = request.get_json()
+    id_veiculo = dados.get('id_veiculo')
+    data =  dados.get('data')
+    valor_total = float(dados.get('valor_total'))
+
+    tipo_usuario = descobre_tipo_usuario()
+
+    if tipo_usuario != 0:
+        return jsonify({'mensagem': 'Apenas Adm pode cadastrar'}), 403
+
+    try:
+        cursor = con.cursor()
+
+        cursor.execute(""" select id_veiculo from veiculo where id_veiculo = ?
+        """, (id_veiculo,))
+        veiculo= cursor.fetchone()
+
+        if not veiculo:
+            return jsonify({'mensagem': 'Veiculo não encontrado',}), 400
+        print(data)
+
+        cursor.execute("""insert into manutencao ( id_veiculo, data, valor_total) 
+                          values(?,?, ?)""", (veiculo[0], data, valor_total))
+        con.commit()
+        return jsonify({'mensagem': 'Manutenção cadastrado com sucesso',}), 200
+
+    except Exception as e:
+        return jsonify({'mensagem': f'Erro ao cadastrar Manutenção'}), 500
+    finally:
+        cursor.close()
 
 
+@app.route('/edicao_manutencao/<int:id_manutencao>', methods=['PUT'])
+def edicao_manutencao(id_manutencao):
+    tipo_usuario = descobre_tipo_usuario()
+
+    if tipo_usuario != 0:
+        return jsonify({'mensagem': 'Apenas Adm pode editar'}), 403
+
+    cursor = con.cursor()
+
+    cursor.execute("""select id_manutencao, id_veiculo, data, valor_total       
+                        from manutencao where id_manutencao=?""", (id_manutencao,))
+    existe_manutencao = cursor.fetchone()
+    if not existe_manutencao:
+        return jsonify({'mensagem': 'Não existe manutencao'})
+    print('sim')
+
+    dados = request.get_json()
+    id_veiculo = dados.get('id_veiculo')
+    data = dados.get('data')
+    valor_total = float(dados.get('valor_total'))
+    try:
+        cursor= con.cursor()
+        cursor.execute("""update manutencao 
+                        set id_veiculo = ? , data = ?, valor_total = ? 
+                        where id_manutencao = ?""",
+                       (id_veiculo, data, valor_total, id_manutencao))
+        con.commit()
+        return jsonify({
+            'mensagem': 'Manutenção atualizado com sucesso',}), 201
+    except Exception as e:
+        return jsonify({'mensagem': 'erro ao editar'})
+    finally:
+        cursor.close()
+
+@app.route('/deletar_manutencao/<int:id_manutencao>', methods=['DELETE'])
+def deletar_manutencao(id_manutencao):
+    tipo_usuario = descobre_tipo_usuario()
+
+    if tipo_usuario != 0:
+        return jsonify({'mensagem': 'Apenas Adm pode editar'}), 403
+
+    cursor = con.cursor()
+    cursor.execute("""select id_manutencao, id_veiculo, valor_total, data       
+                        from manutencao where id_manutencao=?""", (id_manutencao,))
+    existe_manutencao = cursor.fetchone()
+    if not existe_manutencao:
+        return jsonify({'mensagem': 'Não existe manutenção'})
+    try:
+        cursor = con.cursor()
+        cursor.execute("""delete from manutencao where id_manutencao=?""",
+                       (id_manutencao,))
+        con.commit()
+        return jsonify({'mensagem': 'Manutenção deletado com sucesso'})
+    except Exception as e:
+        return jsonify({'mensagem': 'erro ao deletar manutenção em mais de uma tabela'})
+    finally:
+        cursor.close()
+
+@app.route('/adicionar_item_manutencao', methods=['POST'])
+def adicionar_item_manutencao():
+    dados = request.get_json()
+    id_servico = dados.get('id_servico')
+    id_manutencao =  dados.get('id_manutencao')
+    quantidade = int(dados.get('quantidade'))
+
+    tipo_usuario = descobre_tipo_usuario()
+
+    if tipo_usuario != 0:
+        return jsonify({'mensagem': 'Apenas Adm pode cadastrar'}), 403
+
+    try:
+        cursor = con.cursor()
+
+        cursor.execute(""" select id_servico, valor_unitario from servico where id_servico=? """,(id_servico, ))
+        servico_banco = cursor.fetchone()
+        valor_unitario = servico_banco[1]
+
+        valor_multi = float(valor_unitario*quantidade)
 
 
+        cursor.execute(""" select id_manutencao from manutencao where id_manutencao=?""",(id_manutencao,))
+        manutencao_banco = cursor.fetchone()
 
 
+        cursor.execute("""insert into item_manutencao ( id_manutencao, id_servico, quantidade, valor_total) 
+                          values(?,?,?,?) returning id_item_manutencao""", (manutencao_banco[0],servico_banco[0],quantidade, valor_multi))
+
+        con.commit()
+        id_item_manutencao = cursor.fetchone()[0]
 
 
+        cursor.execute("""UPDATE MANUTENCAO SET VALOR_TOTAL = COALESCE(MANUTENCAO.VALOR_TOTAL,0) + (SELECT ITEM_MANUTENCAO.VALOR_TOTAL  
+                                                                FROM ITEM_MANUTENCAO
+                                                               WHERE ITEM_MANUTENCAO.ID_MANUTENCAO = MANUTENCAO.ID_MANUTENCAO
+                                                               AND ITEM_MANUTENCAO.ID_ITEM_MANUTENCAO = ?)
+                           WHERE MANUTENCAO.ID_MANUTENCAO  = ? """,(id_item_manutencao,))
+        con.commit()
+        return jsonify({'mensagem': 'Item de manutencao cadastrado com sucesso',}), 200
 
-
-
+    except Exception as e:
+        return jsonify({'mensagem': f'Erro ao cadastrar Item Manutenção'}), 500
+    finally:
+        cursor.close()
 
